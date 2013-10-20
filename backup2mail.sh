f1b25bba8ded4dffc3a04c3f9a4d64814608ef15
Rodolfo Pilas <rodolfo@pilas.net>
2013-10-20 21:58:53 -0200

    backup2mail.sh arrangements

--- backup2mail.sh
+++ backup2mail.sh
@@ -3,8 +3,11 @@
 # 
 # -> requieres package biabam
  
-# At the same directory requires $FILELIST with a file/folders to backup one per line
-FILELIST=respaldar.txt
+# ## CHANGE HERE ### 
+TO="rootway@email-gmail.com" # e-mail to send backup files
+FILELIST=respaldar.txt       # file at the same directory with file/folders to backup
+
+# ## NO more change needed from here ##
 if [ -h $0 ]; then
    RESPARCHIVO=`dirname $(readlink $0)`/$FILELIST;
 else
@@ -14,28 +17,22 @@ if [ ! -f $RESPARCHIVO ]; then
    echo "$RESPARCHIVO file not found" | /usr/bin/mail -s "$HOSTNAME Error" $TO; exit 1
 fi
  
-TO="rootway@email-gmail.com"  # CHANGE IT !!
- 
 DATE=$(date +%Y%m%d-%H%M)
 DIRTMP=$RANDOM
 DIRDEST=/tmp/$DIRTMP/$HOSTNAME-$DATE
- 
 mkdir -p $DIRDEST
- 
 dpkg --get-selections > $DIRDEST/dpkg-selections   # Debian Package Manager !
 for RESPALDAR in $(cat $RESPARCHIVO); do
    cp -r --preserve --parents $RESPALDAR $DIRDEST
 done
 cd $DIRDEST
 tar czf /tmp/$DIRTMP/$HOSTNAME-$DATE.tgz .
- 
 if [ -x /usr/bin/biabam ]; then
    echo "Backup $HOSTNAME of $DATE is attached" | /usr/bin/biabam /tmp/$DIRTMP/$HOSTNAME-$DATE.tgz \
        -s "BACKUP $HOSTNAME of $DATE" $TO
 else
    echo "biabam utility is not installed, apt-get install biabam" | /usr/bin/mail -s "$HOSTNAME Error" $TO
 fi
- 
 rm -R /tmp/$DIRTMP
 exit 0
 
