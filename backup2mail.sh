9124da60a6b9f1f35231476ff876bcf209e56cc0
Rodolfo Pilas <rodolfo@pilas.net>
2013-10-20 23:12:54 -0200

    More arrangements of script

--- backup2mail.sh
+++ backup2mail.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 # Create backup of files (tgz) and send it to an e-mail account to storage 
 # 
+# -> requires full working MTA (smtp) server at localhost & mail command
 # -> requieres package biabam
  
 # ## CHANGE HERE ### 
-TO="rootway@email-gmail.com" # e-mail to send backup files
-FILELIST=respaldar.txt       # file at the same directory with file/folders to backup
+TO="rootway@email-gmail.com"   # e-mail to send backup files
+FILELIST=respaldar.txt         # file at the same directory with file/folders to backup
 
 # ## NO more change needed from here ##
 if [ -h $0 ]; then
@@ -14,25 +15,26 @@ else
    RESPARCHIVO=`dirname $0`/$FILELIST;
 fi
 if [ ! -f $RESPARCHIVO ]; then
-   echo "$RESPARCHIVO file not found" | /usr/bin/mail -s "$HOSTNAME Error" $TO; exit 1
+   echo "$RESPARCHIVO file not found" | /usr/bin/mail -s "$HOSTNAME Error" $TO
+   exit 1
 fi
- 
 DATE=$(date +%Y%m%d-%H%M)
 DIRTMP=$RANDOM
 DIRDEST=/tmp/$DIRTMP/$HOSTNAME-$DATE
-mkdir -p $DIRDEST
-dpkg --get-selections > $DIRDEST/dpkg-selections   # Debian Package Manager !
-for RESPALDAR in $(cat $RESPARCHIVO); do
-   cp -r --preserve --parents $RESPALDAR $DIRDEST
-done
-cd $DIRDEST
-tar czf /tmp/$DIRTMP/$HOSTNAME-$DATE.tgz .
 if [ -x /usr/bin/biabam ]; then
+   mkdir -p $DIRDEST
+   dpkg --get-selections > $DIRDEST/dpkg-selections   # Debian Package Manager !
+   for RESPALDAR in $(cat $RESPARCHIVO); do
+      cp -r --preserve --parents $RESPALDAR $DIRDEST
+   done
+   cd $DIRDEST
+   tar czf /tmp/$DIRTMP/$HOSTNAME-$DATE.tgz .
    echo "Backup $HOSTNAME of $DATE is attached" | /usr/bin/biabam /tmp/$DIRTMP/$HOSTNAME-$DATE.tgz \
        -s "BACKUP $HOSTNAME of $DATE" $TO
+   rm -R /tmp/$DIRTMP
 else
    echo "biabam utility is not installed, apt-get install biabam" | /usr/bin/mail -s "$HOSTNAME Error" $TO
+   exit 2
 fi
-rm -R /tmp/$DIRTMP
 exit 0
 
